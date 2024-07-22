950012c7a9bfa9e7c1376d40630b072aacefc5d0
Sebastien Escorne <152779537+SebasTFC@users.noreply.github.com>
2024-07-22 19:28:07 +0200

    Ajout des images,header et footer

---
+++ include/footer.php
@@ -0,0 +1,27 @@
+
+<footer class="bg-dark text-secondary fond-de-page">
+        <div class="row text-center">
+            <div class="col-12 col-lg-4">
+                <h5 class="text-secondary">Nos horaires</h5>
+                <p>Ouvert tous les jours de l'année <br/>
+                De 8h30 à 18h <br/>
+                </p>
+            </div>
+            <div class="col-6  col-lg-4">
+                <p>Zoo Arcadia <br/>
+                    3250, Route de Broceliande <br/>
+                    35200 Brest <br/>
+                </p>
+            </div>
+            <div class="col-6  col-lg-4">
+                <p>zoo.arcadia@gmail.com<br/>
+                  01.22.33.44.55 <br/>
+                </p>
+            </div>
+        </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+</body>
+</html>
